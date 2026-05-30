f428ae1b3161c5d41b56dbeb9116428ce28c285b
Memory <mehmetfokur1144@gmail.com>
2026-05-30 16:09:27 +0300

    Element buffer object and texture implementation

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <filesystem>
 #include <iostream>
 
+#include "stb_image.h"
 #include "Shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -33,34 +34,69 @@ int main() {
 
 	Shader shader("/shaders/basic.vert", "/shaders/basic.frag");
 
-	float vertices[] = { -0.75f, 0.75f, 0.0f, 0.5f, 0.75f, 0.25f,
-		-0.75f, -0.75f, 0.0f, 0.5f, 0.25f, 0.75f,
-		0.0f, -0.75f, 0.0f, 0.5f, 0.25f, 0.75f
+	float vertices[] = {
+		-0.75f, 0.75f, 0.0f, 1.0f, 0.75f, 0.25f, 1.0f, 0.0f, // top left
+		-0.75f, -0.75f, 0.0f, 0.5f, 0.55f, 0.75f, 0.0f, 0.0f, // bottom left
+		0.0f, -0.75f, 0.0f, 0.5f, 0.55f, 0.75f, 0.0f, 1.0f, // bottom right
+		0.0f, 0.75f, 0.0f, 1.0f, 0.75f, 0.25f, 1.0f, 1.0f // top right
+	};
+	
+	unsigned int indices[] = {
+		0, 1, 2,
+		0, 2, 3
 	};
 
-	unsigned int VAO, VBO;
+
+	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, channel_number;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load((std::filesystem::current_path().string() + "/textures/container.jpg").c_str(), &width, &height, &channel_number, 0);
+
+	if (data) {
+		GLenum format = channel_number == 4 ? GL_RGBA : GL_RGB;
+
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		std::cout << "Texture couldn't be loaded. stbi: " << stbi_failure_reason() << std::endl;
+	}
+
+	stbi_image_free(data);
 
 	while (!glfwWindowShouldClose(window)) {
-		glClearColor(0.0f, 0.1f, 0.1f, 1.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBindTexture(GL_TEXTURE_2D, texture);
 		shader.use();
-		float time = (float)glfwGetTime();
-		float brightnessValue = (sin(time) + 1.0f) / 2.0f;
-		shader.setFloat("brightness", brightnessValue);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
--- shaders/basic.frag
+++ shaders/basic.frag
@@ -1,9 +1,11 @@
 #version 330 core
 in vec3 vertexColor;
+in vec2 texCoord;
+
 out vec4 FragColor;
 
-uniform float brightness;
+uniform sampler2D texture;
 
 void main() {
-	FragColor = vec4(vertexColor * brightness, 1.0);
+	FragColor = texture(texture, texCoord) * vec4(vertexColor, 1.0);
 }
